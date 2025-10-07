a44bc21ab83640292ba6c0690bf2c59dbeefc839
anselmocouto <55468149+anselmocouto@users.noreply.github.com>
2025-10-07 15:29:31 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 React Demo com CI/CD</h1>
+        <p>Pipeline automatizado com GitHub Actions</p>
+        <p>
+          Editar <code>src/App.js</code> e salvar para recarregar.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
